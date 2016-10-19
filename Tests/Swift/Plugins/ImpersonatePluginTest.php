2e2ddb72a0115cad4f1afad63d78086c5a9c1397
Dejan Angelov <angelovdejan92@gmail.com>
2016-10-19 12:15:03 +0200

    Test impersonating multiple senders

--- Tests/Swift/Plugins/ImpersonatePluginTest.php
+++ Tests/Swift/Plugins/ImpersonatePluginTest.php
@@ -21,19 +21,11 @@ class ImpersonatePluginTest extends \PHPUnit_Framework_TestCase
 {
     public function testImpersonatesSender()
     {
-        $message = $this->prophesize('\Swift_Mime_Message');
         $oldAddress = 'old@example.com';
         $newAddress = 'new@example.com';
 
+        $message = $this->prepareMessage();
         $message->getFrom()->willReturn([$oldAddress]);
-
-        $message->setFrom(Argument::type('string'))->will(function ($args) {
-            $this->getFrom()->willReturn([$args[0]]);
-        });
-
-        $message->setFrom(Argument::type('array'))->will(function ($args) {
-            $this->getFrom()->willReturn($args[0]);
-        });
 
         $headers = $this->prophesize('\Swift_Mime_HeaderSet');
         $message->getHeaders()->willReturn($headers->reveal());
@@ -47,13 +39,64 @@ class ImpersonatePluginTest extends \PHPUnit_Framework_TestCase
 
         $plugin = new ImpersonatePlugin($newAddress);
 
-        $event = $this->prophesize('\Swift_Events_SendEvent');
-        $event->getMessage()->willReturn($message->reveal());
+        $event = $this->prepareSendEvent($message->reveal());
 
         $plugin->beforeSendPerformed($event->reveal());
         self::assertSame([$newAddress], $message->reveal()->getFrom());
 
         $plugin->sendPerformed($event->reveal());
         self::assertSame([$oldAddress], $message->reveal()->getFrom());
+    }
+
+    public function testImpersonatesMultipleSenders()
+    {
+        $oldAddresses = ['old_1@example.com', 'old_2@example.com'];
+        $newAddress = 'new@example.com';
+
+        $message = $this->prepareMessage();
+        $message->getFrom()->willReturn($oldAddresses);
+
+        $headers = $this->prophesize('\Swift_Mime_HeaderSet');
+        $message->getHeaders()->willReturn($headers->reveal());
+
+        $header = $this->prophesize('\Swift_Mime_Header');
+        $header->getFieldBodyModel()->willReturn($oldAddresses);
+
+        $headers->addMailboxHeader('X-Swift-From', $oldAddresses)->will(function () use ($header) {
+            $this->get('X-Swift-From')->willReturn($header->reveal());
+        });
+
+        $plugin = new ImpersonatePlugin($newAddress);
+
+        $event = $this->prepareSendEvent($message->reveal());
+
+        $plugin->beforeSendPerformed($event->reveal());
+        self::assertSame([$newAddress], $message->reveal()->getFrom());
+
+        $plugin->sendPerformed($event->reveal());
+        self::assertSame($oldAddresses, $message->reveal()->getFrom());
+    }
+
+    private function prepareMessage()
+    {
+        $message = $this->prophesize('\Swift_Mime_Message');
+
+        $message->setFrom(Argument::type('string'))->will(function ($args) {
+            $this->getFrom()->willReturn([$args[0]]);
+        });
+
+        $message->setFrom(Argument::type('array'))->will(function ($args) {
+            $this->getFrom()->willReturn($args[0]);
+        });
+
+        return $message;
+    }
+
+    private function prepareSendEvent($message)
+    {
+        $event = $this->prophesize('\Swift_Events_SendEvent');
+        $event->getMessage()->willReturn($message);
+
+        return $event;
     }
 }
